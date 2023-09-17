6c399fa99c8478ebca8c19bdb6adb818f81755a4
zb78 <zb78@hotmail.fr>
2023-09-17 20:29:50 +0200

    solution fuite memoire avec unsubscribe

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,29 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
-import { interval } from 'rxjs';
-import { tap, take } from "rxjs/operators";
+import { Subject, interval } from 'rxjs';
+import { tap, take, takeUntil } from "rxjs/operators";
 
 @Component({
   selector: 'app-face-snap-list',
   templateUrl: './face-snap-list.component.html',
   styleUrls: ['./face-snap-list.component.scss']
 })
-export class FaceSnapListComponent implements OnInit {
+export class FaceSnapListComponent implements OnInit, OnDestroy {
 
   faceSnaps!: FaceSnap[];
+  private destroy$!: Subject<boolean>;
 
   constructor(private faceSnapsService: FaceSnapsService) { }
 
   ngOnInit(): void {
+    this.destroy$ = new Subject<boolean>();
     this.faceSnaps = this.faceSnapsService.getAllFaceSnaps();
 
     interval(1000).pipe(
-      take(1),
+      // take(3),
+      takeUntil(this.destroy$),
       tap(console.log)
       ).subscribe();
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+  }
 
 
 }
