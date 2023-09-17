af1e9fb94131b21867c27f15a3cf7c72a68f498b
zb78 <zb78@hotmail.fr>
2023-09-17 11:53:04 +0200

    observable haut niveau mergeMap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, interval } from 'rxjs';
-import { map , filter, tap } from 'rxjs/operators';
+import { interval, of } from 'rxjs';
+import { concatMap, mergeMap, delay, exhaustMap, map, switchMap, take, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,19 +9,30 @@ import { map , filter, tap } from 'rxjs/operators';
 })
 export class AppComponent implements OnInit {
 
-
-  interval$!: Observable<string>;
+  redTrainsCalled = 0;
+  yellowTrainsCalled = 0;
 
   ngOnInit() {
-    this.interval$ = interval(1000).pipe(
-      filter((value) => value % 3 === 0),
-      map((value) => value % 2 === 0 ? `Je suis ${value} et je suis pair` : `Je suis ${value} et je suis impair` ),
-      tap(text => this.logger(text))
+    interval(500).pipe(
+      take(10),
+      map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
+      tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
+      mergeMap(color => this.getTrainObservable$(color)),
+      tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
+    ).subscribe();
+  }
+
+  getTrainObservable$(color: 'rouge' | 'jaune') {
+    const isRedTrain = color === 'rouge';
+    isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
+    const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
+    console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
+    return of({ color, trainIndex }).pipe(
+      delay(isRedTrain ? 5000 : 6000)
     );
   }
 
-  logger(text: string) {
-    console.log(`Log: ${text}`)  ;
-
+  translateColor(color: 'rouge' | 'jaune') {
+    return color === 'rouge' ? 'red' : 'yellow';
   }
 }
